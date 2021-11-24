cb57a65ed18a826d059ba862e2c9146ff6b1cda9
Vanessa Pertsch <lottscha@gmail.com>
2021-11-24 08:26:46 +0100

    add db file

---
+++ social.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS users;
+
+ CREATE TABLE users(
+     id SERIAL PRIMARY KEY,
+     first VARCHAR(255) NOT NULL CHECK (first != ''),
+     last VARCHAR(255) NOT NULL CHECK (last != ''),
+     email VARCHAR(255) NOT NULL UNIQUE CHECK (email != ''),
+     password VARCHAR(255) NOT NULL,
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+ );
